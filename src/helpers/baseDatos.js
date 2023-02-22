b97767e506455c20cb5c7601515560f7783e7219
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-02-22 08:45:16 -0500

    Se agrega imagenes y practica de DOM

---
+++ src/helpers/baseDatos.js
@@ -0,0 +1,39 @@
+//Crear una base de datos.
+
+export let productos = Array(
+    {
+        nombre:"producto1",
+        descripcion:"descripcion",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/Mt10_gris.png?alt=media&token=706ee5e8-9e2e-4b26-a2a4-46b64fe4de26","na2"),
+        precio: 12000000,
+        popularidad:5
+    },
+    {
+        nombre:"producto2",
+        descripcion:"descripcion",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/ducati.png?alt=media&token=594f1b4f-d285-472a-a3aa-3b81c0288bfc","na2"),
+        precio: 12000000,
+        popularidad:5
+    },
+    {
+        nombre:"producto3",
+        descripcion:"descripcion",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/kawasaki%20H2R1.png?alt=media&token=1cd93df5-e3d1-4885-846c-3b14466c86b4","na"),
+        precio: 12000000,
+        popularidad:5
+    },
+    {
+        nombre:"producto4",
+        descripcion:"descripcion",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/kawasaki%20H2R2.png?alt=media&token=aa28c148-b98c-48cc-9125-f8b63e4ef686","na2"),
+        precio: 12000000,
+        popularidad:5
+    },
+    {
+        nombre:"producto5",
+        descripcion:"descripcion",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/yamaha%20R71.jpg?alt=media&token=c052aaf8-fb55-4d12-8c61-0491dfbf39bc","na2"),
+        precio: 12000000,
+        popularidad:5
+    },
+)
