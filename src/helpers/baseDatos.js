36a524ca488f5a3be85cb0e64be4cb36f2c637c4
audio <audio@academicos.cesde.edu.co>
2023-02-22 11:13:21 -0500

    actualizacion de fotos

--- src/helpers/baseDatos.js
+++ src/helpers/baseDatos.js
@@ -4,35 +4,70 @@ export let productos = Array(
     {
         nombre:"producto1",
         descripcion:"descripcion",
-        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/Mt10_gris.png?alt=media&token=706ee5e8-9e2e-4b26-a2a4-46b64fe4de26","na2"),
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/DucatiBlanca.png?alt=media&token=5bd29589-ae85-4ad8-bc8a-80bab1e07df0","na2"),
         precio: 12000000,
         popularidad:5
     },
     {
         nombre:"producto2",
         descripcion:"descripcion",
-        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/ducati.png?alt=media&token=594f1b4f-d285-472a-a3aa-3b81c0288bfc","na2"),
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/MT09-AZUL.webp?alt=media&token=ee8f2ef1-b9d0-49bd-85af-995a27b4668a","na2"),
         precio: 12000000,
         popularidad:5
     },
     {
         nombre:"producto3",
         descripcion:"descripcion",
-        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/kawasaki%20H2R1.png?alt=media&token=1cd93df5-e3d1-4885-846c-3b14466c86b4","na"),
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/Mt10_negra.png?alt=media&token=049abf1d-69a3-48a5-8c4f-b77c0e9da370","na"),
         precio: 12000000,
         popularidad:5
     },
     {
         nombre:"producto4",
         descripcion:"descripcion",
-        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/kawasaki%20H2R2.png?alt=media&token=aa28c148-b98c-48cc-9125-f8b63e4ef686","na2"),
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/Ninja%20H2R.png?alt=media&token=56fcfc27-9b61-40df-9539-b130ad2971f8","na2"),
         precio: 12000000,
         popularidad:5
     },
     {
         nombre:"producto5",
         descripcion:"descripcion",
-        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/yamaha%20R71.jpg?alt=media&token=c052aaf8-fb55-4d12-8c61-0491dfbf39bc","na2"),
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/R1-AZUL.jpg?alt=media&token=31efb175-fe34-49ca-b098-8a61f3bc04ee","na2"),
+        precio: 12000000,
+        popularidad:5
+    },
+    {
+        nombre:"producto5",
+        descripcion:"descripcion",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/Z900.png?alt=media&token=fa3a5a46-fc72-4736-bdf2-923b091c279a","na2"),
+        precio: 12000000,
+        popularidad:5
+    },
+    {
+        nombre:"producto5",
+        descripcion:"descripcion",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/DUKE8902.webp?alt=media&token=f5d92605-e883-4d33-a501-4f1b45ac5edc","na2"),
+        precio: 12000000,
+        popularidad:5
+    },
+    {
+        nombre:"producto5",
+        descripcion:"descripcion",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/mt03_gris.png?alt=media&token=57b4955d-61e8-4b7a-b077-f7e4ee5fd2ca","na2"),
+        precio: 12000000,
+        popularidad:5
+    },
+    {
+        nombre:"producto5",
+        descripcion:"descripcion",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/mt07-black.webp?alt=media&token=aa8de9f1-9bd0-4c1e-910c-43b80e00d49c","na2"),
+        precio: 12000000,
+        popularidad:5
+    },
+    {
+        nombre:"producto5",
+        descripcion:"descripcion",
+        fotos:Array("https://firebasestorage.googleapis.com/v0/b/katienda.appspot.com/o/r15-gris.webp?alt=media&token=095bc053-36e8-4503-a2c3-7dfa8e71cf94","na2"),
         precio: 12000000,
         popularidad:5
     },
