fa00cefc2b7f5e2ab3585ae50016bcf345fb4e0d
Fenix2902 <alexandercalleagudelo@gmail.com>
2023-03-11 16:42:00 -0500

    se organiza en controllercompras, que no puedan ingresar datos negativos

--- src/controllers/controladorCompra.js
+++ src/controllers/controladorCompra.js
@@ -2,15 +2,35 @@
 let infoProducto = JSON.parse(localStorage.getItem("producto"));
 console.log(infoProducto)
 
-let foto=document.getElementById("foto");
+let foto = document.getElementById("foto");
 foto.src = infoProducto.foto
 
-let nombre=document.getElementById("nombre");
-nombre.textContent=infoProducto.nombre
+let nombre = document.getElementById("nombre");
+nombre.textContent = infoProducto.nombre
 
-let precio=document.getElementById("precio")
-precio.textContent=infoProducto.precio
+let precio = document.getElementById("precio")
+precio.textContent = infoProducto.precio
 
-let descripcion=document.getElementById("descripcion")
-descripcion.textContent=infoProducto.descripcion
+let descripcion = document.getElementById("descripcion")
+descripcion.textContent = infoProducto.descripcion
 
+
+cantidad.addEventListener('keydown', (evento) => {
+    if (evento.key === '-' || evento.key === 'Subtract') {
+        evento.preventDefault();
+    }
+});
+
+
+function subtotal(){
+    let cantidad = document.getElementById("cantidad")
+    let precio = document.getElementById("precio")
+
+    let subtotal= cantidad*precio
+    return subtotal
+}
+
+// subtotal,(precio,cantidad)=>{
+//  let subtotal = precio*cantidad
+//  return subtotal
+// }
